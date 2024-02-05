5630c98e3ebb43822dc75cbeefcff8db3b94d608
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 09:35:02 +0100

    Oude eindopdrachten toegevoegd

---
+++ script.js
@@ -0,0 +1,7 @@
+function tohtmlcss0() {
+    location.href = "https://b2c264ce-0348-4bf4-ae13-8fc2efbc7dff-00-3cdie6xmdmysk.worf.replit.dev/index.html"
+}
+
+function tohtmlcss1() {
+    location.href = "https://7496022e-0f98-44a6-b54f-924ccd73d2f9-00-setgsqcbqm70.riker.repl.co/"
+}
